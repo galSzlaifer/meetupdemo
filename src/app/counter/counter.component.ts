0fcb0e7a15d97613ed2a6d8c3a8c818ad4afe261
Gal Szlaifer <99748911+galSzlaifer@users.noreply.github.com>
2022-12-14 17:02:06 +0200

    update to angular 15

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable, Subscription,timeout} from "rxjs";
+
 
 @Component({
   selector: 'app-counter',
@@ -7,13 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CounterComponent implements OnInit {
   public count: number = 0;
-
+  //countObservable:Observable<{number:0}>
+  //fierst
   constructor() { }
 
   ngOnInit(): void {
   }
 
   add() {
+    //performance.mark("add-started", {
+    //  detail: "bla bla"
+    //});
+
+    //setTimeout(()=>{                           // <<<---using ()=> syntax
+    //  console.log('go to BE and update the number');
+    //  this.count++;
+   //   //performance.measure('add-started');
+    //}, 3000);
     this.count++;
   }
 
